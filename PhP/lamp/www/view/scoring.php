847fb866c1c67ce19bc19a1d90cab487e36acc3f
MrFlooch <pn31vgu@eduvaud.ch>
2024-12-03 17:11:39 +0100

    feat(addPoint): le joueur qui marque le point est marqué comme avoir marqué le point

--- PhP/lamp/www/view/scoring.php
+++ PhP/lamp/www/view/scoring.php
@@ -1,14 +1,16 @@
 <?php
-$title = 'Score du match '.$game->number;
+$title = 'Score du match ' . $game->number;
 
 ob_start();
 ?>
 
-<div class="row text-center"><h1>Set <?= $set->number ?></h1></div>
+<div class="row text-center">
+    <h1>Set <?= $set->number ?></h1>
+</div>
 <div class="d-flex flex-row justify-content-around">
 
     <!-- 1ère équipe -->
-    <div class="d-flex flex-column order-<?= (($game->toss+$set->number) % 2 == 0) ? 1 : 2 ?>">
+    <div class="d-flex flex-column order-<?= (($game->toss + $set->number) % 2 == 0) ? 1 : 2 ?>">
         <div class="teamname"><?= $game->receivingTeamName ?></div>
         <div class="setscore"><?= $game->scoreReceiving ?> sets</div>
         <div class="setscore"><?= count($game->receivingTimeouts) ?> timeouts</div>
@@ -17,65 +19,82 @@ ob_start();
             <form method="post" action="?action=scorePoint" id="actionForm">
                 <input type="hidden" name="setid" value="<?= $set->id ?>" />
                 <input type="hidden" name="receiving" value="1" />
-                <input type="hidden" name="playerId" id="selectedPlayerId" value="" />
+                <input type="hidden" name="playerId" id="selectedPlayerId1" value="" />
 
+                <!-- Boutons pour afficher les joueurs-->
                 <div class="d-flex flex-column align-items-stretch">
                     <?php foreach ($receivingPositions as $player) : ?>
-                        <button 
-                            type="button" 
-                            class="btn btn-primary mb-2 w-100 player-button" 
+                        <button
+                            type="button"
+                            class="player"
                             data-player-id="<?= $player->id ?>"
-                            data-player-name="<?= $player->number ?> <?= $player->last_name ?>">
+                            data-player-name="<?= $player->number ?> <?= $player->last_name ?>"
+                            onclick="scoreBy1(<?= $player->id ?>)">
                             <?= $player->number ?> <?= $player->last_name ?>
                         </button>
                     <?php endforeach; ?>
                 </div>
-                
+
                 <!-- Boutons pour ajouter un point ou une faute -->
-                <div id="actionButtons" class="mt-3" style="display: none;">
+                <div id="actionButtons" class="mt-3">
                     <button type="submit" name="action" value="point" class="btn btn-success w-100 mb-2">Ajouter un point</button>
-                    <button type="submit" name="action" value="fault" class="btn btn-danger w-100">Enregistrer une faute</button>
+                    <div class="d-flex flex-row justify-content-between">
+                        <a class="btn btn-danger m-2" href="?action=selectBooking&teamid=<?= $game->receivingTeamId ?>&setid=<?= $set->id ?>">
+                            Sanctions
+                        </a>
+                        <?php if (count($game->receivingTimeouts) < 2) : ?>
+                            <a class="btn btn-secondary m-2" href="?action=timeout&teamid=<?= $game->receivingTeamId ?>&setid=<?= $set->id ?>" <?= count($game->receivingTimeouts) > 1 ? "disabled" : "" ?>>
+                                Temps Mort
+                            </a>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </form>
         </div>
-
-
     </div>
 
     <!-- 2ème équipe -->
-    <div class="d-flex flex-column order-<?= (($game->toss+$set->number) % 2 == 0) ? 2 : 1 ?>">
+    <div class="d-flex flex-column order-<?= (($game->toss + $set->number) % 2 == 0) ? 2 : 1 ?>">
         <div class="teamname"><?= $game->visitingTeamName ?></div>
-        <div class="setscore"><?= $game->scoreVisiting ?></div>
+        <div class="setscore"><?= $game->scoreVisiting ?> sets</div>
         <div class="setscore"><?= count($game->visitingTimeouts) ?> timeouts</div>
         <div class="score"><?= $set->scoreVisiting ?></div>
 
         <div class="row actions d-flex flex-column">
-    <form method="post" action="?action=scorePoint" id="actionForm">
-        <input type="hidden" name="setid" value="<?= $set->id ?>" />
-        <input type="hidden" name="receiving" value="0" />
-        <input type="hidden" name="playerId" id="selectedPlayerId" value="" />
+            <form method="post" action="?action=scorePoint" id="actionForm">
+                <input type="hidden" name="setid" value="<?= $set->id ?>" />
+                <input type="hidden" name="receiving" value="0" />
+                <input type="hidden" name="playerId" id="selectedPlayerId2" value="" />
 
-        <div class="d-flex flex-column align-items-stretch">
-            <?php foreach ($visitingPositions as $player) : ?>
-                <button 
-                    type="button" 
-                    class="btn btn-primary mb-2 w-100 player-button" 
-                    data-player-id="<?= $player->id ?>"
-                    data-player-name="<?= $player->number ?> <?= $player->last_name ?>">
-                    <?= $player->number ?> <?= $player->last_name ?>
-                </button>
-            <?php endforeach; ?>
+                <div class="d-flex flex-column align-items-stretch">
+                    <?php foreach ($visitingPositions as $player) : ?>
+                        <button
+                            type="button"
+                            class="player"
+                            data-player-id="<?= $player->id ?>"
+                            data-player-name="<?= $player->number ?> <?= $player->last_name ?>"
+                            onclick="scoreBy2(<?= $player->id ?>)">
+                            <?= $player->number ?> <?= $player->last_name ?>
+                        </button>
+                    <?php endforeach; ?>
+                </div>
+
+                <!-- Boutons pour ajouter un point ou une faute -->
+                <div id="actionButtons" class="mt-3">
+                    <button type="submit" name="action" value="point" class="btn btn-success w-100 mb-2">Ajouter un point</button>
+                    <div class="d-flex flex-row justify-content-between">
+                        <a class="btn btn-danger m-2" href="?action=selectBooking&teamid=<?= $game->receivingTeamId ?>&setid=<?= $set->id ?>">
+                            Sanctions
+                        </a>
+                        <?php if (count($game->receivingTimeouts) < 2) : ?>
+                            <a class="btn btn-secondary m-2" href="?action=timeout&teamid=<?= $game->receivingTeamId ?>&setid=<?= $set->id ?>" <?= count($game->receivingTimeouts) > 1 ? "disabled" : "" ?>>
+                                Temps Mort
+                            </a>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </form>
         </div>
-        
-        <!-- Boutons pour ajouter un point ou une faute -->
-        <div id="actionButtons" class="mt-3" style="display: none;">
-            <button type="submit" name="action" value="point" class="btn btn-success w-100 mb-2">Ajouter un point</button>
-            <button type="submit" name="action" value="fault" class="btn btn-danger w-100">Enregistrer une faute</button>
-        </div>
-    </form>
-</div>
-
-
     </div>
 </div>
 
@@ -83,3 +102,14 @@ ob_start();
 $content = ob_get_clean();
 require_once 'gabarit.php';
 ?>
+
+<script>
+    function scoreBy1(playerid)
+    {
+        selectedPlayerId1.value = playerid
+    }
+    function scoreBy2(playerid)
+    {
+        selectedPlayerId2.value = playerid
+    }
+</script>
